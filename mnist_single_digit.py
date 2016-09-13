481321cdc86964c32465066481d1d7e21a551156
Aravind Battaje <aravind@oxidification.com>
2016-09-13 19:56:23 +0530

    Add tensorboard summary stuff to MNIST single digit model

--- mnist_single_digit.py
+++ mnist_single_digit.py
@@ -8,32 +8,49 @@ from dataset.load_mnist import load_mnist
 def main(_):
     mnist = load_mnist()
 
+    def make_summary(name, var):
+        with tf.name_scope('summary'):
+            tf.scalar_summary('mean/' + name, tf.reduce_mean(var))
+            tf.scalar_summary('max/' + name, tf.reduce_max(var))
+            tf.scalar_summary('min/' + name, tf.reduce_min(var))
+            tf.histogram_summary(name, var)
+
     def conv_graph(images):
         with tf.name_scope('conv1'):
             weights = tf.Variable(tf.truncated_normal([5, 5, 1, 32], stddev=0.1), name='weights')
+            make_summary(weights.name, weights)
             biases = tf.Variable(tf.zeros([32]), name='biases')
+            make_summary(biases.name, biases)
             conv1 = tf.nn.relu(tf.nn.conv2d(images, weights, strides=[1, 1, 1, 1], padding='SAME') + biases)
             pool1 = tf.nn.max_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
         with tf.name_scope('conv2'):
             weights = tf.Variable(tf.truncated_normal([5, 5, 32, 64], stddev=0.1), name='weights')
+            make_summary(weights.name, weights)
             biases = tf.Variable(tf.zeros([64]), name='biases')
+            make_summary(biases.name, biases)
             conv2 = tf.nn.relu(tf.nn.conv2d(pool1, weights, strides=[1, 1, 1, 1], padding='SAME') + biases)
             pool2 = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
         with tf.name_scope('hidden1'):
             pool2_flattened = tf.reshape(pool2, [-1, 7 * 7 * 64])
             weights = tf.Variable(tf.truncated_normal([7 * 7 * 64, 1024], stddev=0.01), name='weights')
+            make_summary(weights.name, weights)
             biases = tf.Variable(tf.zeros([1024]), name='biases')
+            make_summary(biases.name, biases)
             hidden1 = tf.nn.relu(tf.matmul(pool2_flattened, weights) + biases)
         with tf.name_scope('readout'):
             weights = tf.Variable(tf.truncated_normal([1024, 10], stddev=0.01), name='weights')
+            make_summary(weights.name, weights)
             biases = tf.Variable(tf.zeros([10]), name='biases')
+            make_summary(biases.name, biases)
             logits = tf.matmul(hidden1, weights) + biases
+            tf.histogram_summary('logits', logits)
         
         return logits
     
     def loss_graph(logits, labels):
         cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits, labels, name='cross_entropy')
         loss = tf.reduce_mean(cross_entropy, name='cross_entropy_mean')
+        tf.scalar_summary('cross_entropy_mean', loss)
         return loss
 
     def train_graph(loss):
@@ -45,7 +62,7 @@ def main(_):
         correct = tf.nn.in_top_k(logits, labels, 1)
         return tf.reduce_sum(tf.cast(correct, tf.float32))
 
-    with tf.Graph().as_default():
+    with tf.Graph().as_default() as graph:
         tf_train_images = tf.placeholder(tf.float32, [128, 28, 28, 1])
         tf_train_labels = tf.placeholder(tf.int32, [128])
 
@@ -53,11 +70,15 @@ def main(_):
         loss = loss_graph(logits, tf_train_labels)
         train_step = train_graph(loss)
         evaluation = evaluate_graph(logits, tf_train_labels)
+
+        merged_summaries = tf.merge_all_summaries()
+
+        summary_writer = tf.train.SummaryWriter('logs/mnist_single_digit', graph)
     
         with tf.Session() as sess:
             tf.initialize_all_variables().run()
 
-            for step in xrange(15000):
+            for step in xrange(3000):
                 offset = (step * 128) % (mnist.train_images.shape[0] - 128)
                 images = mnist.train_images[offset:(offset + 128), :, :, None]
                 labels = mnist.train_labels[offset:(offset + 128)]
@@ -78,9 +99,11 @@ def main(_):
                 if step % 100 == 0:
                     print "Step {}: loss = {}, valid_accuracy = {}".format(
                         step, loss_value, evaluate_on_dataset(mnist.valid_images, mnist.valid_labels))
+                    summary = sess.run(merged_summaries, feed_dict=feed_dict)
+                    summary_writer.add_summary(summary, step)
 
-                if (step + 1) % 1000 == 0:      
-                    print "train_accuracy = {}".format(evaluate_on_dataset(mnist.train_images, mnist.train_labels))
+                if step % 999 == 0:
+                    #print "train_accuracy = {}".format(evaluate_on_dataset(mnist.train_images, mnist.train_labels))
                     print "test_accuracy = {}".format(evaluate_on_dataset(mnist.test_images, mnist.test_labels))
 
 if __name__ == '__main__':
